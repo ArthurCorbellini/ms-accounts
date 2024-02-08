0d906496b2705c74be091f88f60dbd97707d8684
Arthur Corbellini <arthur.corbellini27@gmail.com>
2024-02-07 22:46:36 -0300

    default spring project

---
+++ src/test/java/com/corbellini/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corbellini.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
